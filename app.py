924c976ad0dc765cf3fc80e4107d0e993f5cc793
Issa Victoria Peñas <111822151+IVPENAS@users.noreply.github.com>
2023-05-26 20:28:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ def load_model():
   model=tf.keras.models.load_model('weights-improvement-16-0.89.hdf5')
   return model
 model=load_model()
-st.title("🍏🍅Apples Tomatoes Classifier")
+st.title(" Cat and Dog Classifier 🐱🐶")
+st.write("Final Exam: Model Deployment in the Cloud")
 file=st.file_uploader("Choose a photo from your computer",type=["jpg","png"])
 
 def import_and_predict(image_data,model):
@@ -29,3 +30,17 @@ else:
     class_names=['Apple', 'Tomato']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
+
+    
+
+
+#Final Exam: Model Deployment in the Cloud
+
+#![n-cats-a-20181226](https://github.com/IVPENAS/Embed_FinalProj/assets/111822151/07779232-ea8c-4e2f-8a9f-09dba009c803)
+
+#Created By:
+#- Peñas, Issa Victoria H.
+#- Villamor, Earl Kristian G.
+
+#<p align = "justify"> Due to our interest about animals specifically Cats and Dogs, allowed us to create a custom dataset to our Classifier utilized on Convolutional Neural Network learning model which is designed to distinguish using [number of ssize] pictures between the mentioned animals. The Classfier starts by inputting a image into the network which it'll be processed through multiple layers of convolutional filters allowing to distinguish patterns and features present in the inputted image, in which the network will gradually determine the different characteristics of a cat and dog including [1]<b><i> ears</i></b>, [2]]<b><i> tail</i></b>, [3]<b><i> snout</i></b>, or [4]<b><i> eyes</i></b> which by then inputed to the fully conncted layers consolidating the informmation and making predictions about the image's class. By modifying the weights and biases of its network based on the discrepancies between predicted and real labels throughout the training phase will result an improvement upon the classifier. The model can iteratively adjust its parameters through a method called backpropagation to increase its accuracy over time. Once the model was trained and completed the classifier can be inputted images from the user and output the preditions whether the images contains a cat or a dog.
+#</p>
