d578af07e8fe2fcd568146bf957151a829372304
Issa Victoria Peñas <111822151+IVPENAS@users.noreply.github.com>
2023-05-26 18:23:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
-#Deploy
-
 import streamlit as st
 import tensorflow as tf
-
-@st.cache(allow_output_mutation=True)
-def load_model():
-  model=tf.keras.models.load_model('model.h5')
-  return model
-model=load_model()
-
-st.write("""
-# Cats vs. Dogs"""
-)
-
-file=st.file_uploader("Choose animal photo from computer",type=["jpg","png"])
-
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
+
+@st.cache_resource
+def load_model():
+  model=tf.keras.models.load_model('weights-improvement-16-0.89.hdf5')
+  return model
+model=load_model()
+st.title("🍏🍅Apples Tomatoes Classifier")
+file=st.file_uploader("Choose a photo from your computer",type=["jpg","png"])
+
 def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+    size=(128,128)
+    image = ImageOps.fit(image_data,size, Image.LANCZOS)
+    image = np.asarray(image)
+    image = image / 255.0
+    img_reshape = np.reshape(image, (1, 128, 128, 3))
+    prediction = model.predict(img_reshape)
     return prediction
 if file is None:
     st.text("Please upload an image file")
@@ -31,6 +26,6 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['cat','dog']
+    class_names=['Apple', 'Tomato']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
